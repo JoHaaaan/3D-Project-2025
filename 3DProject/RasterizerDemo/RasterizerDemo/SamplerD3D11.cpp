53479ec7c1ead7630283daf4e32065fe76766377
Johan <johan.wisell52@gmail.com>
2025-11-06 08:50:34 +0100

    :)

--- 3DProject/RasterizerDemo/RasterizerDemo/SamplerD3D11.cpp
+++ 3DProject/RasterizerDemo/RasterizerDemo/SamplerD3D11.cpp
@@ -2,17 +2,24 @@
 
 SamplerD3D11::SamplerD3D11(ID3D11Device* device, D3D11_TEXTURE_ADDRESS_MODE adressMode, std::optional<std::array<float, 4>> borderColour)
 {
+
 }
 
 SamplerD3D11::~SamplerD3D11()
 {
+    if (sampler)
+    {
+        sampler->Release();
+        sampler = nullptr;
+    }
 }
 
 void SamplerD3D11::Initialize(ID3D11Device* device, D3D11_TEXTURE_ADDRESS_MODE adressMode, std::optional<std::array<float, 4>> borderColour)
 {
+
 }
 
 ID3D11SamplerState* SamplerD3D11::GetSamplerState() const
 {
-	return nullptr;
+	return sampler;
 }
