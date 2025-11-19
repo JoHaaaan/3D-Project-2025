b0e663c73b0bc6b868eca4f54997cbfc28fa09b4
JoHaaaan <johan.wisell52@gmail.com>
2025-11-19 15:06:42 +0100

    SubMeshD3D11.cpp
    0 clue om denna fungerar

--- 3DProject/RasterizerDemo/RasterizerDemo/SubMeshD3D11.cpp
+++ 3DProject/RasterizerDemo/RasterizerDemo/SubMeshD3D11.cpp
@@ -2,23 +2,29 @@
 
 void SubMeshD3D11::Initialize(size_t startIndexValue, size_t nrOfIndicesInSubMesh, ID3D11ShaderResourceView* ambientTextureSRV, ID3D11ShaderResourceView* diffuseTextureSRV, ID3D11ShaderResourceView* specularTextureSRV)
 {
+	startIndex = startIndexValue;
+	nrOfIndices = nrOfIndicesInSubMesh;
+	ambientTexture = ambientTextureSRV;
+	diffuseTexture = diffuseTextureSRV;
+	specularTexture = specularTextureSRV;
 }
 
 void SubMeshD3D11::PerformDrawCall(ID3D11DeviceContext* context) const
 {
+	context->DrawIndexed(static_cast<UINT>(nrOfIndices), static_cast<UINT>(startIndex), 0);
 }
 
 ID3D11ShaderResourceView* SubMeshD3D11::GetAmbientSRV() const
 {
-	return nullptr;
+	return ambientTexture;
 }
 
 ID3D11ShaderResourceView* SubMeshD3D11::GetDiffuseSRV() const
 {
-	return nullptr;
+	return diffuseTexture;
 }
 
 ID3D11ShaderResourceView* SubMeshD3D11::GetSpecularSRV() const
 {
-	return nullptr;
+	return specularTexture;
 }
