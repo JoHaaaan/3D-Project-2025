f2a257198f67b2556fa58bcc74d978a0bbc3dc51
JoHaaaan <johan.wisell52@gmail.com>
2025-11-20 09:34:33 +0100

    Implement IndexBufferD3D11 resource management
    Added proper initialization and release of the D3D11 index buffer resource. The class now tracks the number of indices and returns the correct buffer pointer.

--- 3DProject/RasterizerDemo/RasterizerDemo/IndexBufferD3D11.cpp
+++ 3DProject/RasterizerDemo/RasterizerDemo/IndexBufferD3D11.cpp
@@ -2,22 +2,47 @@
 
 IndexBufferD3D11::IndexBufferD3D11(ID3D11Device* device, size_t nrOfIndicesInBuffer, uint32_t* indexData)
 {
+	Initialize(device, nrOfIndicesInBuffer, indexData);
 }
 
 IndexBufferD3D11::~IndexBufferD3D11()
 {
+	if (buffer)
+	{
+		buffer->Release();
+		buffer = nullptr;
+	}
 }
 
 void IndexBufferD3D11::Initialize(ID3D11Device* device, size_t nrOfIndicesInBuffer, uint32_t* indexData)
 {
+	if (buffer)
+	{
+		buffer->Release();
+		buffer = nullptr;
+	}
+
+	nrOfIndices = nrOfIndicesInBuffer;
+	D3D11_BUFFER_DESC desc = {};
+	desc.ByteWidth = static_cast<UINT>(nrOfIndicesInBuffer * sizeof(uint32_t));
+	desc.Usage = D3D11_USAGE_DEFAULT;
+	desc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	desc.CPUAccessFlags = 0;
+	desc.MiscFlags = 0;
+	desc.StructureByteStride = 0;
+
+	D3D11_SUBRESOURCE_DATA initData = {};
+	initData.pSysMem = indexData;
+
+	device->CreateBuffer(&desc, &initData, &buffer);
 }
 
 size_t IndexBufferD3D11::GetNrOfIndices() const
 {
-	return size_t();
+	return nrOfIndices;
 }
 
 ID3D11Buffer* IndexBufferD3D11::GetBuffer() const
 {
-	return nullptr;
+	return buffer;
 }
