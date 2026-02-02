18c5f35873f985404f8c02e526a9553eeea7892f
Johan <johan.wisell52@gmail.com>
2026-02-02 11:52:42 +0100

    d

--- 3DProject/RasterizerDemo/RasterizerDemo/PixelShader.hlsl
+++ 3DProject/RasterizerDemo/RasterizerDemo/PixelShader.hlsl
@@ -14,17 +14,17 @@ cbuffer MaterialBuffer : register(b2)
 
 struct PS_INPUT
 {
-    float4 clipPosition : SV_POSITION;
+    float4 clipPosition  : SV_POSITION;
     float3 worldPosition : WORLD_POSITION;
-    float3 worldNormal : NORMAL;
-    float2 uv : TEXCOORD0;
+    float3 worldNormal   : NORMAL;
+    float2 uv            : TEXCOORD0;
 };
 
 struct PS_OUTPUT
 {
-    float4 Albedo : SV_Target0;    // RGB: diffuse color, A: ambient strength
-    float4 Normal : SV_Target1;    // RGB: world normal (packed), A: specular strength
-    float4 Extra : SV_Target2;     // RGB: world position, A: specular power
+    float4 Albedo : SV_Target0;
+    float4 Normal : SV_Target1;
+    float4 Extra  : SV_Target2;
 };
 
 Texture2D shaderTexture : register(t0);
